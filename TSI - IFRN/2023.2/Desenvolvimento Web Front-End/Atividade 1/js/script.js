3b4e627c11d71cff3dc27e338bb52a9890e27582
Aristides Neto <aristidesjbn@gmail.com>
2023-09-05 13:43:33 -0300

    Primeiro commit

---
+++ TSI - IFRN/2023.2/Desenvolvimento Web Front-End/Atividade 1/js/script.js
@@ -0,0 +1,12 @@
+function questao1(){
+  let distancia = Number(prompt("Qual a distância percorrida em KM?"))
+  let gasolina = Number(prompt("Quanto de gasolina foi gasta em litros?"))
+
+  let gasto = distancia/gasolina
+  alert(`Consumo de ${gasto}/L`)
+}
+
+function questao2(){
+  let hoje = new Date()
+
+}
