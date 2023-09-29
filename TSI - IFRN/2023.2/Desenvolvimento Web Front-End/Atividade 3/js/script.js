48d83d6259fe1360e0f068cd7f7ae106560cd570
Aristides Neto <aristidesjbn@gmail.com>
2023-09-28 21:39:26 -0300

    Atividade 3 Completa(5/5)

--- TSI - IFRN/2023.2/Desenvolvimento Web Front-End/Atividade 3/js/script.js
+++ TSI - IFRN/2023.2/Desenvolvimento Web Front-End/Atividade 3/js/script.js
@@ -1,4 +1,19 @@
 function questao1(){
+    var operação = Number(prompt('informe qual operação você deseja fazer: \n\n 1- Soma \n 2- Subtração \n 3- Multiplicação \n 4- Divisão'));
+    var n1 = Number(prompt('informe o primeiro número:'));
+    var n2 = Number(prompt('informe o segundo número:'));
+    
+    switch (operação) {
+        case 1:
+            return alert(`${n1} + ${n2} = ${n1+n2}`);
+        case 2: 
+            return alert(`${n1} - ${n2} = ${n1-n2}`);
+        case 3:
+            return alert(`${n1} * ${n2} = ${n1*n2}`);
+        case 4: 
+            return alert(`${n1} / ${n2} = ${n1/n2}`);
+        default:
+            return alert(`O número informado não condiz com as operações mostradas`);    }
 
 }
 function questao2(){
@@ -27,15 +42,80 @@ function questao2(){
         }
         i++
     }
-    console.log(`a) Preço total da compra: ${preco_total}`)
-    console.log(`b) Produto mais caro: ${mais_caro} || Produto mais barato: ${mais_barato}`)
+    alert(`a) Preço total da compra: ${preco_total}`)
+    alert(`b) Produto mais caro: ${mais_caro} || Produto mais barato: ${mais_barato}`)
 }
 function questao3(){
 
+    var senha = prompt("Digite a senha:")
+    valida_senha(senha);
+
+
+    function valida_senha(senhaF){
+        var regEx  = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/; //Andressa quem ensinou.
+        var regExToF = regEx.test(senhaF)
+
+        if (regExToF){
+            alert('Senha cadastrada.');
+        }else{
+            alert('A senha deve conter no mínimo 8 dígitos, letras maíusculas, minúsculas e números');
+            questao3();
+        }
+    }
 }
 function questao4(){
+    var i = 1;
+    var soma = 0;
 
+    function lançar_dado(quant){
+        while (i <= quant){
+            d6 =  Math.floor(1 + Math.random() * 5);
+            alert(`${i}º dado: ${d6}`);
+            soma = soma+d6
+            i++
+        }   
+        alert(`A soma dos ${quant} dados foi ${soma}`)
+    }
+    
+
+    var qtd_d6 = prompt("Digite a quantidade de dados:")
+    lançar_dado(qtd_d6);
 }
 function questao5(){
 
+    function contar_vogais(palavra){
+
+        let i = 0;
+        const tamanho_palavra = palavra.length;
+        var cont = 0;
+    
+        while (i < tamanho_palavra){
+    
+            const letra = palavra.charAt(i);
+
+            if (verifica_vogal(letra)){
+                   cont++
+            }
+            i++
+        }
+        alert(`A palavra ${palavra} contém ${cont} vogais`)
+    }
+    
+    function verifica_vogal(letra){
+        letra = letra.toLowerCase();
+            
+        switch (letra) {
+            case "a": case "à": case "â": case "ã":
+            case "e": case "é": case "ê":
+            case "i": case "í":
+            case "o": case "ó": case "ô":
+            case "u": case "ú":
+                return resposta = true;
+            default:
+                return resposta = false;
+        }
+    }
+    
+    var palavra = prompt("Digite a palavra:")
+    contar_vogais(palavra);
 }
